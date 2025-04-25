3a530b03d46ef4c3fd8c968a32b484cd9be68fda
wenzexu <ricol.xwz@outlook.com>
2025-04-26 09:45:49 +1000

    fix: 更新分析脚本的源地址和网站ID

--- overrides/javascripts/analysis.js
+++ overrides/javascripts/analysis.js
@@ -2,10 +2,10 @@ function umami() {
   if (window.location.hostname !== "localhost" && window.location.hostname !== "127.0.0.1") {
     var script = document.createElement("script");
     script.defer = true;
-    script.src = "https://umami.ricolxwz.io/script.js";
+    script.src = "http://35.95.99.18:45862/script.js";
     script.setAttribute(
       "data-website-id",
-      "3a5faee0-96f2-4bf6-b74e-ab3ae685794a"
+      "168d0646-2ebc-439e-a419-905fc93f2ce0"
     );
     document.head.appendChild(script);
   }
