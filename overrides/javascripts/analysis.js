10334958be693f3f8fe909d6a1df06f5c7ca9506
wenzexu <ricol.xwz@outlook.com>
2025-04-26 10:16:04 +1000

    update analysis.js

--- overrides/javascripts/analysis.js
+++ overrides/javascripts/analysis.js
@@ -2,7 +2,7 @@ function umami() {
   if (window.location.hostname !== "localhost" && window.location.hostname !== "127.0.0.1") {
     var script = document.createElement("script");
     script.defer = true;
-    script.src = "http://umami.ricolxwz.io/script.js";
+    script.src = "https://umami.ricolxwz.io/script.js";
     script.setAttribute(
       "data-website-id",
       "168d0646-2ebc-439e-a419-905fc93f2ce0"
