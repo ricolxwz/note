3c82a7db93b75ae1f5d7f1660fb4ca3eced8e522
wenzexu <ricol.xwz@outlook.com>
2026-09-23 21:38:43 +0800

    fix(editor): 修正编辑链接地址

--- overrides/javascripts/web-editor.js
+++ overrides/javascripts/web-editor.js
@@ -1,7 +1,7 @@
 // Open the current note in github.dev, including after instant navigation.
 function updateWebEditorLinks() {
   const sourcePrefix = "https://github.com/ricolxwz/note/edit/master/docs/";
-  const editorPrefix = "https://github.dev/ricolxwz/note/blob/master/docs/";
+  const editorPrefix = "https://github.com/ricolxwz/note/blob/master/docs/";
   document.querySelectorAll('a.md-content__button[rel="edit"]').forEach((link) => {
     if (link.href.startsWith(sourcePrefix)) {
       link.href = editorPrefix + link.href.slice(sourcePrefix.length);
